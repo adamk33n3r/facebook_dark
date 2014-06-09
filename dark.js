3333607e5e697ce8e6940d270141f574410ba4d3
Adam Keenan <adam_keenan@taylor.edu>
2014-06-09 08:04:56 -0400

    Good for now. Have almost found everything

--- dark.js
+++ dark.js
@@ -1,8 +1,3 @@
-// Checking for page title
-if (document.title.indexOf("Facebook" != -1)) {
-    // Create button
-    //var btn = $("<button>");
-    //btn.text("click meeeee you fraud");
-    // Append to DOM
-    //$("body").append(btn);
-}
+$(function() {
+//    $(".cardRightCol").remove();
+});
