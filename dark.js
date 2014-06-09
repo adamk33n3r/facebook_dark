38a6c8ff2fcea0a158a9019f1a2ffb85707b3fd7
Adam Keenan <adam_keenan@taylor.edu>
2014-06-08 21:22:02 -0400

    Initial commit

---
+++ dark.js
@@ -0,0 +1,8 @@
+// Checking for page title
+if (document.title.indexOf("Facebook" != -1)) {
+    // Create button
+    //var btn = $("<button>");
+    //btn.text("click meeeee you fraud");
+    // Append to DOM
+    //$("body").append(btn);
+}
